2d165abb5e00c2f8b60e2b4c0554bf584ea54ab1
alvarosc2000 <152218487+alvarosc2000@users.noreply.github.com>
2024-09-10 20:50:59 +0200

    comentario no necesario

--- public/controladores/companyController.js
+++ public/controladores/companyController.js
@@ -6,8 +6,7 @@ async function registroEmpresa(user, pass, email) {
         // Llamar a la función de registro de usuario del controlador de usuario
         const userId = await registro_usuario(user, pass, email, 'company');
 
-        // No necesitas insertar la empresa aquí, ya que la función de registro de usuario
-        // ya se encarga de insertar los datos de la empresa en la tabla Compania
+
 
         return 'Registro de empresa exitoso';
     } catch (error) {
